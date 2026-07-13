db5d2845e3e901612c18cc1c971dbbe31a8d7f11
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-07-13 10:45:37 +0545

    add authMiddleware in the module's router itself instead of global router

--- backend/src/modules/comments/comments.routes.ts
+++ backend/src/modules/comments/comments.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import commentController from "./comments.controller.js";
+import authMiddleware from "../auth/auth.middleware.js";
 
 export const nestedCommentRoutes = Router({ mergeParams: true });
 nestedCommentRoutes.get('/', commentController.getCommentsByPostId);
-nestedCommentRoutes.post('/', commentController.postComment);
+nestedCommentRoutes.post('/', authMiddleware, commentController.postComment);
 
 export const flatCommentRoutes = Router();
-flatCommentRoutes.delete('/:commentId', commentController.deleteComment);
+flatCommentRoutes.delete('/:commentId', authMiddleware, commentController.deleteComment);
 
--- backend/src/modules/posts/posts.routes.ts
+++ backend/src/modules/posts/posts.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import postController from "./posts.controller.js";
+import authMiddleware from "../auth/auth.middleware.js";
 
 const router = Router();
 
 router.get('/feed', postController.getFeed);
-router.post('/', postController.createPost)
+router.post('/', authMiddleware, postController.createPost)
 router.get('/:slug', postController.getPostBySlug);
-router.delete('/:id', postController.deletePost)
+router.delete('/:id', authMiddleware, postController.deletePost)
 
 export default router;
