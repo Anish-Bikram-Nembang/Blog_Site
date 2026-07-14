26ebfe2fcdd76330a122bd06e133a68b5994f33f
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-07-14 19:05:51 +0545

    mount categories router and add a create category endpoint

--- backend/src/modules/categories/categories.repository.ts
+++ backend/src/modules/categories/categories.repository.ts
@@ -5,16 +5,36 @@ interface Category {
   name: string;
 }
 interface CategoriesRepository {
+  getCategoryByName(categoryName: string): Promise<Category | null>
+  createCategory(categoryName: string): Promise<Category>
   getAllCategories(): Promise<Category[]>
   getCategoryById(id: string): Promise<Category | null>
 }
 
 const categoriesRepository: CategoriesRepository = {
+  async getCategoryByName(categoryName: string) {
+    const result = await pool.query(`
+      SELECT
+        category_id AS "categoryId",
+        name
+      FROM categories
+      WHERE name = $1
+    `, [categoryName]);
+    return result.rows[0] ?? null;
+  },
+  async createCategory(categoryName: string) {
+    const result = await pool.query(`
+      INSERT INTO categories (name)
+      VALUES ($1) RETURNING category_id AS "categoryId",
+      name
+    `, [categoryName]);
+    return result.rows[0];
+  },
   async getAllCategories() {
     const result = await pool.query(`
       SELECT
         category_id AS "categoryId",
-        name AS "name",
+        name
       FROM categories
     `);
     return result.rows;
@@ -23,9 +43,9 @@ const categoriesRepository: CategoriesRepository = {
     const result = await pool.query(`
       SELECT
         category_id AS "categoryId",
-        name AS "name",
+        name
       FROM categories
-      WHERE id = $1
+      WHERE category_id = $1
     `, [id]);
     return result.rows[0] ?? null;
   }
